70853a8b985446ca36312033b94b06d8972a4fcd
Isaiah Leapheart <ijleapheart19@students.desu.edu>
2022-09-23 14:07:10 -0400

    passed

--- cypher.py
+++ cypher.py
@@ -2,7 +2,7 @@
 # CSCI-121 Elements of Computer Programming II
 # Recitation 1 - Encryption with a password
 # *******************************************************
-
+import math
 import string
 
 alphabet = string.printable
@@ -11,9 +11,26 @@ ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+    password = math.ceil(len(message) / len(password)) * password
+    for index, ch in enumerate(message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch = (ord_of_ch + key) % len(alphabet)
+        encrypted_ch = alphabet[shifted_ord_of_ch]
+        encrypted_message += encrypted_ch
+    return encrypted_message
 
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    encrypted_message = ''
+    password = math.ceil(len(message) / len(password)) * password
+    for index, ch in enumerate(message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch = (ord_of_ch - key) % len(alphabet)
+        encrypted_ch = alphabet[shifted_ord_of_ch]
+        encrypted_message += encrypted_ch
+    return decrypted_message
